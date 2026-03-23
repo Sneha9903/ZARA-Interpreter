6aeb2175e1263b83dc2bcbaa17d8daed38b29723
Sneha9903 <snehashaw9903@gmail.com>
2026-03-24 00:31:17 +0530

    Create TokenType.java

---
+++ src/TokenType.java
@@ -0,0 +1,14 @@
+public enum TokenType {
+	// Keywords
+    SET, SHOW, WHEN, LOOP,
+    
+    // Literals and Identifiers
+    NUMBER, STRING, IDENTIFIER,
+    
+    // Operators and Punctuation
+    EQUALS, PLUS, MINUS, STAR, SLASH, 
+    GREATER, LESS, COLON, NEWLINE,
+    
+    // End of File
+    EOF
+}
