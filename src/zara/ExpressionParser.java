ff72ff7b57f60ede8f975e3a635aa6c07f0be1a8
Ompalyadav-beep <yompal233@gmail.com>
2026-04-14 16:33:52 +0530

    Fixed parser according to tokenizer (removed unsupported tokens)

--- src/zara/ExpressionParser.java
+++ src/zara/ExpressionParser.java
@@ -1,7 +1,5 @@
 package zara;
 
-import java.beans.Expression;
-
 public class ExpressionParser {
     private final ParserContext context;
     public ExpressionParser(ParserContext context) {
@@ -10,13 +8,13 @@ public class ExpressionParser {
     public Expression parse() {
         return parseExpression();
     }
-    private Expression parseExpression() {
+    public Expression parseExpression() {
     return parseEquality();
 }
     private Expression parseEquality() {
         Expression expression = parseComparison();
 
-        while (context.match(TokenType.EQUAL_EQUAL, TokenType.BANG_EQUAL)) {
+        while (context.match(TokenType.EQUAL_EQUAL)) {
             Token operator = context.previous();
             Expression right = parseComparison();
             expression = new BinaryOpNode(expression, operator.getValue(), right);
@@ -27,8 +25,7 @@ public class ExpressionParser {
 
     private Expression parseComparison(){
         Expression expression=parseTerm();
-        while (context.match(TokenType.GREATER, TokenType.GREATER_EQUAL,
-                TokenType.LESS, TokenType.LESS_EQUAL)) {
+        while (context.match(TokenType.GREATER, TokenType.LESS)) {
             Token operator = context.previous();
             Expression right = parseTerm();
             expression = new BinaryOpNode(expression, operator.getValue(), right);
@@ -49,7 +46,7 @@ public class ExpressionParser {
 
     private Expression parseFactor(){
         Expression expression=parseUnary();
-        while (context.match(TokenType.STAR, TokenType.SLASH)) {
+        while (context.match(TokenType.MULTIPLY, TokenType.DIVIDE)) {
             Token operator = context.previous();
             Expression right = parseUnary();
             expression = new BinaryOpNode(expression, operator.getValue(), right);
@@ -78,12 +75,6 @@ public class ExpressionParser {
 
         if (context.match(TokenType.IDENTIFIER)) {
             return new VariableNode(context.previous().getValue());
-        }
-
-        if (context.match(TokenType.LEFT_PAREN)) {
-            Expression expression = parseExpression();
-            context.consume(TokenType.RIGHT_PAREN, "Expected ')' after expression");
-            return expression;
         }
 
         throw context.error("Expected number, string, identifier, or '(' but found: " + context.peek().getType());
